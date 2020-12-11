9d43e11758d68bdc928b57e655e1882db05555d5
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2020-12-11 19:32:59 +0900

    Add Flask send AssetBundle to HoloLens

--- app.py
+++ app.py
@@ -5,10 +5,22 @@ import werkzeug
 from datetime import datetime
 import subprocess
 import shutil
+import configparser
+import hololens
+import shlex
 
 
 app = Flask(__name__)
 
+# read config file and create HoloLens instance
+config = configparser.ConfigParser()
+config.read('config.ini')
+holo_conf = config['HoloLens']
+upload = config['Upload']
+unity = config['Unity']
+# create instance
+hololens2 = hololens.Hololens(
+    holo_conf['username'], holo_conf['password'], holo_conf['ipaddress'])
 
 cmd1 = "python rgb2grayscale.py ./Media/Uploads/Chair/sketch/p1"
 cmd2 = "python main.py --test --data_dir ../../../Media/Uploads/Chair/ --train_dir ../Checkpoint/Chair/ --test_dir ../../../Media/Output --sketch_views FS"
@@ -27,8 +39,8 @@ def upload_files():
 
         os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-        if os.path.exists("Media/Ply"):
-            shutil.rmtree("Media/Ply")
+        # if os.path.exists("Media/Ply"):
+        #     shutil.rmtree("Media/Ply")
 
         upload_files = request.files.getlist('file')
         for file in upload_files:
@@ -39,17 +51,26 @@ def upload_files():
 
         print("exsists : {}".format(os.path.exists(app.config['UPLOAD_FOLDER'])))
 
+        # rgb2grayscale
         subprocess.call(cmd1.split())
+
+        # calc depth and normal map
         os.chdir("Network/code/MonsterNet")
         subprocess.call(cmd2.split())
+        # create point cloud and poisson surface reconstruction
         os.chdir("../../../Fusion/output/ReconstructMesh/Win32/Release")
         subprocess.call(cmd3.split(), shell=True)
+        # ply2obj
         os.chdir("../../../../../")
         subprocess.call(cmd4.split())
+
+        # AssetBundle send hololens
+        hololens2.upload(upload['directory'], upload['filename'])
 
         # Delete Upload image directory and normal,depth map
         shutil.rmtree(app.config['UPLOAD_FOLDER'])
         shutil.rmtree("Media/Output")
+
         # return mesh file
         return send_file(filename_or_fp="Media/Ply/p1/mesh.obj", as_attachment=True)
 
@@ -60,7 +81,7 @@ def upload_files():
             <title> 3Dオブジェクト生成 </title>
         </head> 
         <body>
-            <h1> ファイルをアップロードしてオブジェクトを生成します </h1>
+            <h1> スケッチ画像2枚を選択してアップロードしてください。オブジェクトを生成します。 </h1>
             <form method=post enctype=multipart/form-data>
                 <p><input type=file name=file multiple='multiple'>
                 <input type=submit value=Upload>
