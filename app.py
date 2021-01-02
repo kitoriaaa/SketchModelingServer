6fda348a5fdbde2eea230cccb51301974dee3d85
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-01-02 23:59:27 +0900

    Update can create plane, character object

--- app.py
+++ app.py
@@ -26,15 +26,20 @@ hololens2 = hololens.Hololens(
 
 
 cmd2 = "python main.py --test --data_dir ../../../Media/Uploads/Chair/ --train_dir ../Checkpoint/Chair/ --test_dir ../../../Media/Output --sketch_views FS"
-cmd3 = "bash ReconstructMesh.sh"
+# cmd3 = "bash ReconstructMesh.sh"
+
+# reconstruct = "ReconstructMesh.exe 1 F ../../../../../Media/Uploads/Chair/sketch/p1/ ../../../../../Media/Output/images/p1/ ../../../../../Media/Ply/p1/ ../../../../../Media/Uploads/Chair/view/view.off"
+poisson = "PoissonRecon.exe --in ../../../../../Media/ply/p1/points.ply --out ../../../../../Media/Ply/p1/mesh.ply --depth 11 --samplesPerNode 5.0 --pointWeight 0.1"
+
 
 # AssetBundle Build command
 buildAssetBundle = unity['UnityPath'] + " -batchmode -quit -logFile ./build.log -projectPath " + unity['projectPath'] + " -executeMethod " + unity['methodName']
 
-app.config['UPLOAD_FOLDER'] = './Media/Uploads/Chair/sketch/p1/'
+app.config['UPLOAD_FOLDER'] = './Media/Uploads/sketch/p1/'
 app.config['OUTPUT_FOLDER'] = "./Media/Output"
 app.config['PLY_FOLDER'] = "./Media/Ply/p1/mesh.ply"
 
+obj_type_dict = {"plane":"TS", "chair":"FS", "character":"FS"}
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_files():
@@ -49,6 +54,13 @@ def upload_files():
             shutil.rmtree("Media/Ply")
 
         object_type = request.form["obj-type"]
+        if (not (object_type in obj_type_dict.keys())):
+            return make_response(jsonify({"result":"obj-type must be plane or chair or character"}))
+
+        cmd2 = "python main.py --test --data_dir ../../../Media/Uploads/ --train_dir ../Checkpoint/" + object_type + "/ --test_dir ../../../Media/Output --sketch_view " + obj_type_dict[object_type] + " --view_file " + object_type + ".off"
+        reconstruct = "ReconstructMesh.exe 1 " + obj_type_dict[object_type] + " ../../../../." + app.config['UPLOAD_FOLDER'] + " ../../../../../Media/Output/images/p1/ ../../../../../Media/Ply/p1/ ../../../../../Media/Uploads/view/" + object_type + ".off"
+        print(cmd2)
+        print(reconstruct)
         print(object_type)
         upload_files = request.files.getlist('file')
         for file in upload_files:
@@ -67,7 +79,8 @@ def upload_files():
         subprocess.call(cmd2.split())
         # create point cloud and poisson surface reconstruction
         os.chdir("../../../Fusion/output/ReconstructMesh/Win32/Release")
-        subprocess.call(cmd3.split(), shell=True)
+        subprocess.call(reconstruct.split(), shell=True)
+        subprocess.call(poisson.split(), shell=True)
         # ply2obj
         os.chdir("../../../../../")
         ply2obj.convert(app.config['PLY_FOLDER'])
